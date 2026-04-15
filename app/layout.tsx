fdc5dafc57517509a3e61429b728f3ee30a3c478
Irina-Igmm <laurasitraka@yahoo.com>
2026-04-15 18:08:56 +0300

    Ajout du chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Providers } from "./providers";
 
@@ -31,6 +32,14 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <Script
+          src="https://cdn.botpress.cloud/webchat/v3.6/inject.js"
+          strategy="afterInteractive"
+        />
+        <Script
+          src="https://files.bpcontent.cloud/2026/04/15/07/20260415072041-3FYX5LFX.js"
+          strategy="afterInteractive"
+        />
       </body>
     </html>
   );
